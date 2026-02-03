bcb1b0ab51f317d0b8e8f8ba587057f0067ebf0f
Jun Cedrick Narciso <114903875+thisjuncedrick@users.noreply.github.com>
2026-02-03 16:42:51 +0800

    💬 chore-text: update marketing announcements
    * add notice regarding privacy policy updates and analytics
    * announce availability of new photo strip frame sets
    * implement links to privacy and booth pages within announcement bar

--- config/announcement.tsx
+++ config/announcement.tsx
@@ -1,1 +1,18 @@
-export const announcements = [];
+import Link from "next/link";
+
+export const announcements = [
+  <p>
+    We have updated our{" "}
+    <Link href='/privacy' className='font-bold underline'>
+      privacy policy
+    </Link>{" "}
+    to reflect the introduction of privacy-preserving analytics.
+  </p>,
+  <p>
+    New{" "}
+    <Link href='/booth' className='font-bold underline'>
+      photo strip frame sets
+    </Link>{" "}
+    are now available in the photobooth experience.
+  </p>,
+];
